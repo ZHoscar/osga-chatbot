975ec6fd797f4887630c6f59391b0edaf9cc6051
Oscar Chen <b.oscarchen@gmail.com>
2018-03-11 22:31:14 +0800

    good job

--- gogo.py
+++ gogo.py
@@ -38,7 +38,10 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text + "good"))
+        TextSendMessage(text=event.message.text))
+
+
+
 
 
 if __name__ == "__main__":
