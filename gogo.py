eff2baf02aa9f7f3897fafc4c5829bf44a8f3293
Oscar Chen <b.oscarchen@gmail.com>
2018-03-14 22:05:29 +0800

    g

--- gogo.py
+++ gogo.py
@@ -176,7 +176,7 @@ def handle_message(event):
                     ]
                 )
                 )
-    line_bot_api.reply_message(event.reply_token, Carousel_template)
+        line_bot_api.reply_message(event.reply_token, Carousel_template)
 
 
 
