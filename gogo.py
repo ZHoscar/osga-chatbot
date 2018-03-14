d5111b84e138a6f8b90c9fb3c13f18ccf1d7b9f0
Oscar Chen <b.oscarchen@gmail.com>
2018-03-14 12:56:43 +0800

    g

--- gogo.py
+++ gogo.py
@@ -108,7 +108,7 @@ def handle_message(event):
         
 
         line_bot_api.reply_message(
-                                   event.reply_token, TextSendMessage(text=trans.text)
+                                   event.reply_token, TextSendMessage(text=trans.text))
 
 
 
